c09196c3d990bdfb28d16007a21891ec2a381fe2
Tareef-debugger <tareeftoufique@gmail.com>
2025-08-26 17:05:19 +0600

    Add files via upload

---
+++ project.py
@@ -0,0 +1,8 @@
+def intro(countryname):
+    print("I live in ",countryname )
+countryname1 = input("Enter your countrys name")
+intro(countryname1)
+def icapital(capital):
+    print("My countrys capital is",capital )
+user = input("Enter your countrys capital")
+icapital(user)
